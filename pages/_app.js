bc1b7d53f18727bbc27bf2aa9bf85ec57c6f17b3
jianan1104 <linjianan1104@gmail.com>
2022-02-13 16:19:45 +0800

    Add favicon.ico

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Head from "next/head";
 import { Container } from 'semantic-ui-react';
 import MenuComponent from '../components/Menu';
 import { MetaMaskProvider } from "metamask-react";
@@ -8,6 +9,9 @@ import './style.css';
 export default function App({ Component, pageProps }) {
    return (
       <>
+          <Head>
+            <link rel="shortcut icon" href="/favicon.ico" />
+         </Head>
          <MetaMaskProvider>
             <Container>
                <MenuComponent />
