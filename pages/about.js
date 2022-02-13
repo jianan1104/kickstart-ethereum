6c27ca341c66be1c3f5b47a47113a66ae4c2a489
jianan1104 <linjianan1104@gmail.com>
2022-02-13 15:53:37 +0800

    Edit about.js page.

--- pages/about.js
+++ pages/about.js
@@ -1,13 +1,23 @@
 import React from "react";
-import { Button } from "semantic-ui-react";
+import { Button, Icon } from "semantic-ui-react";
+import Highlight from 'react-highlight'
+
 
 const About = () => {
     return(
         <>
+            <link rel="stylesheet" href="node_modules/highlight.js/styles/default.css"></link>
             <h2>About</h2>
+            <p>A blockchain crowdfunding platform using by Solidity 0.8.11 and Next.js</p>
             <h3>Created by <a href="https://www.facebook.com/Linjianan1104/">Jian-An, Lin</a></h3>
             <p>Email: linjianan1104@gmail.com</p>
-            <Button icon="github" basic><a href="https://github.com/jianan1104/kickstart-ethernum">Github Repo</a></Button>
+            <a href="https://github.com/jianan1104/kickstart-ethereum">
+                <Button basic>
+                    <Button.Content visible>
+                        <Icon name='github' />
+                    </Button.Content>
+                </Button>
+            </a>
         </>
         
     )
